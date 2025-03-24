71cc7deed7675c85bcc494f32a5da33c9fa31643
Pal <pal.tilva@gmail.com>
2025-03-24 02:04:38 -0700

    basic main page layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
-import './App.css'
+// App.jsx - Main component
+import React from 'react';
+import Header from './components/header';
+import FlightSearch from './components/FlightSearch';
+import './App.css';
 
 function App() {
-
   return (
-    <>
-      <div>Hi, welcome to skyfare</div>
-    </>
-  )
+    <div className="app">
+      <div className="app-container">
+        {/* Background Image */}
+        <div 
+          className="background-image"
+        />
+        
+        {/* Content */}
+        <div className="content">
+          <Header />
+          <div className="search-container">
+            <FlightSearch />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
